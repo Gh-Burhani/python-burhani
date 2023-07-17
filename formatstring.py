475a2f8f5b733b1f800323a9333cd28f3a0e5cfc
Ghawsuddin-Burhani <burhani.2016@gmail.com>
2023-07-17 19:52:28 +0500

    new lesson about the formatstring

---
+++ formatstring.py
@@ -0,0 +1,9 @@
+year = "2026"
+totalStudents = "5000"
+
+finalResult = "In 2022 number of students were 2400"
+print(finalResult)
+
+finalResult = "In {} number of students were {}"
+
+print(finalResult.format(year, totalStudents))
