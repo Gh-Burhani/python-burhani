12881338367ed973dfafdb3c389438e60e80d513
Ghawsuddin-Burhani <burhani.2016@gmail.com>
2023-07-24 19:47:12 +0500

    Created new files, Dictionaries and if

---
+++ dictionaries.py
@@ -0,0 +1,72 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict)
+
+
+#car = ["Toyata", "Tesla","Honda"] #list
+
+car1 = {
+    "brand":"Toyota",
+    "Model":"Cmry",
+    "year":"2023",
+}
+
+#pairs = key:value
+
+print(car1)
+
+#in dictionaries we dont have Items, we have pairs = key:value
+#Example:
+
+car1 = {
+    "brand":"Toyota",
+    "Model":"Cmry",
+    "year":"2023",
+}
+
+#pairs = key:value
+
+print(car1)
+
+myFriend = {
+    "Name":"Muhammad",
+    "Lastname":"Rahmani",
+    "Phone":"112408009",
+    "Country":"Afghanistan",
+    "Afshanr- Kabul"
+    "Email":"m.u@gmail.com"
+
+}
+
+print(myFriend)
+print(myFriend["Name"])
+print(myFriend["Lastname"])
+print(myFriend["Phone"])
+
+
+
+#Duplicates Not Allowed
+
+car1 = {
+    "brand":"Toyota",
+    "Model":"Cmry",
+    "year":"2023",
+    "year":"1900"
+}
+
+print(car1)
+
+
+#Dictionary Length
+
+print(len(car1))
+
+
+
+#type()
+
+print(type(car1))
---
+++ if.py
@@ -0,0 +1,37 @@
+a = 33
+b = 200
+
+if a < b:
+    print("b is greater than a")
+
+
+    a = 44
+    b = 55
+
+    if b > a:
+        print("b is greater than a")
+
+
+
+a = 44
+b = 55 
+
+if b > a:
+    print(b / a)
+elif a > b:
+    print(a * b)
+
+else:
+    print("Values are not true")
+ 
+
+
+#pass 
+
+if a == b:
+    print("Both are equal")
+
+
+
+
+   
